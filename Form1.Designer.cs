f6800662150a6c5793de96ad96cbf31956ae101c
Wellington Fonseca <wj_fonseca@hotmail.com>
2021-06-08 09:03:24 -0300

    Converter PDF to EXCEL
    Conversor de PDF para Excel em linguagem C#

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace PDF_To_Excel {
+    partial class Janela {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Janela));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(219, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(129, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Converter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(63, 20);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Arquivo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(81, 97);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(209, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Visible = false;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(479, 445);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Feito por: Wellington Juvenal Ferreira Fonseca";
+            // 
+            // Janela
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 85);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Janela";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Conversor de PDF para EXCEL";
+            this.Load += new System.EventHandler(this.Janela_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using Excel = Microsoft.Office.Interop.Excel;
+// feito por WELLINGTON JUVENAL FERREIRA FONSECA
+namespace PDF_To_Excel {
+
+    public partial class Janela : Form {
+        Excel.Application xlApp = new
+            Microsoft.Office.Interop.Excel.Application();
+        public Janela() {
+            InitializeComponent();
+
+        }
+
+        private void Janela_Load(object sender, EventArgs e) {
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+           
+
+         
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+
+            try {
+                ConvertePDF pdftxt = new ConvertePDF();
+                textBox2.Text = pdftxt.ExtrairTexto_PDF(textBox1.Text);
+
+                using (SaveFileDialog sfd = new SaveFileDialog() { Filter = "Excel Workbook|*.xls" }) {
+                    if (sfd.ShowDialog() == DialogResult.OK) {
+                        // Cria uma aplicação em Excel 
+                        Microsoft.Office.Interop.Excel._Application app = new Microsoft.Office.Interop.Excel.Application();
+                        // Cria um novo WorkBook dentro do aplicativo Excel
+                        Microsoft.Office.Interop.Excel._Workbook workbook = app.Workbooks.Add(Type.Missing);
+                        // criando uma nova planilha com aba do excel na pasta de trabalho
+                        Microsoft.Office.Interop.Excel._Worksheet worksheet = null;
+                        // veja a folha de excel por trás do programa  
+                        app.Visible = true;
+                        // obtenha a referência da primeira folha. 
+                        worksheet = workbook.Sheets[1];
+                        // armazenar sua referência à planilha
+                        worksheet = workbook.ActiveSheet;
+                        //   
+                        worksheet.Name = "plan1";
+                        // armazenar parte do cabeçalho no Excel 
+                        Clipboard.Clear();    // limpa valor na area de transferencia        
+                        Clipboard.SetText(textBox2.Text);
+                        worksheet.Paste();
+
+                        // salve a aplicação
+                        workbook.SaveAs(sfd.FileName, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+                        // saia da aplição
+                        app.Quit();
+                    }
+                    
+
+                }
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog()) {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK) {
+                    //obtenha o caminho do arquivo especificado
+                    textBox1.Text = openFileDialog.FileName;
+
+                    //Leia o conteúdo do arquivo em um fluxo
+                    var fileStream = openFileDialog.OpenFile();
+
+
+                }
+            }
+
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e) {
+
+        }
+    }
+    public class ConvertePDF {
+        public string ExtrairTexto_PDF(string caminho) {
+            using (PdfReader leitor = new PdfReader(caminho)) {
+                StringBuilder texto = new StringBuilder();
+                for (int i = 1; i <= leitor.NumberOfPages; i++) {
+                    texto.Append(PdfTextExtractor.GetTextFromPage(leitor, i));
+                }
+                return texto.ToString();
+            }
+        }
+    }
+}
